0fce06dbabeaa52122529c8f0b5c1be5abef143a
unknown <chloebox523@gmail.com>
2024-06-13 18:46:43 -0400

    small velocity update

--- main.py
+++ main.py
@@ -11,23 +11,44 @@ running = True
 size = 32
 
 class Probie():
-  def __init__(self, x, y):
+  def __init__(self, x, y, vel_x, vel_y, grounded):
     self.x = x
     self.y = y
-  
-  def set_x(self, x):
-    self.x = x
+    self.vel_x = vel_x
+    self.vel_y = vel_y
+    self.grounded = grounded
   
   def get_x(self):
     return self.x
 
-  def set_y(self, y):
-    self.y = y
+  def set_x(self, x):
+    self.x = x
   
   def get_y(self):
     return self.y
 
-p = Probie(size, size)
+  def set_y(self, y):
+    self.y = y
+  
+  def get_vel_x(self):
+    return self.vel_x
+
+  def set_vel_x(self, vel_x):
+    self.vel_x = vel_x
+
+  def get_vel_y(self):
+    return self.vel_y
+
+  def set_vel_y(self, vel_y):
+    self.vel_y = vel_y
+
+  def is_grounded(self):
+    return self.grounded
+
+  def set_grounded(self, val):
+    self.grounded = val
+
+p = Probie(size, 10 * size, 0, 0, True)
 
 while running:
     # poll for events
@@ -36,16 +57,22 @@ while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
         running = False
-    if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_LEFT:
-        p.set_x(p.get_x() - size)
-      if event.key == pygame.K_RIGHT:
-        p.set_x(p.get_x() + size)
-      if event.key == pygame.K_UP:
-        p.set_y(p.get_y() - size)
-      if event.key == pygame.K_DOWN:
-        p.set_y(p.get_y() + size)
+  keys = pygame.key.get_pressed()
+  if keys[pygame.K_LEFT]:
+    p.set_x(p.get_x() - size/4)
+  if keys[pygame.K_RIGHT]:
+    p.set_x(p.get_x() + size/4)
+  if keys[pygame.K_UP]:
+    if p.is_grounded():
+      p.set_vel_y(-4 * size/4)
+      p.set_grounded(False)
         
+  
+  if not p.is_grounded():
+    p.set_vel_y(p.get_vel_y() + size/4)
+
+  p.set_x(p.get_x() + p.get_vel_x())
+  p.set_y(p.get_y() + p.get_vel_y())
 
   screen.fill("black")  
   # fill the screen with a color to wipe away anything from last frame
